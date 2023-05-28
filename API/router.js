13260762ebe2c2f8efbf0550997d4df8c8779f6e
Javier-Lug0-03 <a01571142@itesm.mx>
2023-05-28 11:26:43 -0600

    aaa

--- API/router.js
+++ API/router.js
@@ -12,4 +12,23 @@ router.get('/data', (req, res) => {
     })
 })
 
+router.post('/data/:sucursal/:cuenta/:fecha/:hora/:motivo/:tipo/:atendido', (req, res) => {
+    const sucursal = req.params.sucursal;
+    const cuenta = req.params.cuenta;
+    const fecha = req.params.fecha;
+    const hora = req.params.hora;
+    const motivo = req.params.motivo;
+    const tipo = req.params.tipo;
+    const atendido = req.params.atendido;
+    db.query(`INSERT INTO turnos(sucursal,cuenta,fecha,hora,motivo,tipo,atendido) VALUES('${sucursal}','${cuenta}','${fecha}','${hora}','${motivo}','${tipo}','${atendido}')`, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+
+
 module.exports = router;
